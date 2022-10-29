96e1f297057df0b71f7937c94f596a61e1f2c4ff
Charbel Chucri <charbel11chucri@gmail.com>
2022-10-29 17:00:05 +0200

    Fixed preprocessing bug

--- run.py
+++ run.py
@@ -7,13 +7,13 @@ import matplotlib as plt
 def trainModel():
     """
     Trains the model using the training data
-    Returns the trained weights, the MSE and the features that were not deemed useful
+    Returns the trained weights, the MSE, the features that were not deemed useful, the mean of the kept features
     """
 
-    y, features, ids, removed_features = load_training_data()
+    y, features, ids, removed_features, means = load_training_data()
     tx = build_model_data(features)
     w, mse = least_squares(y, tx)
-    return w, mse, removed_features
+    return w, mse, removed_features, means
 
 
 def runModel():
@@ -21,14 +21,28 @@ def runModel():
     Trains the model and then run it on a test set to predict the results
     """
 
-    w, mse_train, removed_features = trainModel()
-    _, features, ids = load_test_data(removed_features)
+    w, mse_train, removed_features, means = trainModel()
+    _, features, ids = load_test_data(removed_features, means)
     tx = build_model_data(features)
     y = np.dot(tx, w)
     y[y < 0] = -1
     y[y >= 0] = 1
 
     create_csv_submission(ids, y, "out.txt")
+
+
+def plot_feature_output_relation():
+    """
+    Shows for each feature x potential relation between x and y
+    If y seems to be random w.r.t. x, the feature is not useful
+    """
+
+    y, features, ids, removed_features = load_training_data()
+
+    features = np.sort(features, axis=0)
+    for i in range(20):
+        plt.plt(features[:, i], y)
+        plt.show()
 
 
 def plot_feature_histogram():
--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ from helpers import *
 def preprocess_train_data(input_data):
     processed_data = []
     removed_features = {}
+    means = []
 
     num_samples = input_data.shape[0]
     for i in range(input_data.shape[1]):
@@ -15,20 +16,22 @@ def preprocess_train_data(input_data):
             removed_features[i] = 1
             continue
 
+
         # Replace missing entries with the mean of available entries then standardize
-        cur_feature[cur_feature == -999] = np.mean(cur_feature[cur_feature != -999])
+        curMean = np.mean(cur_feature[cur_feature != -999])
+        means.append(curMean)
+        cur_feature[cur_feature == -999] = curMean
         standardize(cur_feature)
 
         processed_data.append(cur_feature)
 
-    return np.array(processed_data).T, removed_features
+    return np.array(processed_data).T, removed_features, means
 
 
-def preprocess_test_data(input_data, removed_features):
+def preprocess_test_data(input_data, removed_features, means):
     processed_data = []
 
-    print(len(removed_features))
-
+    realI = 0
     for i in range(input_data.shape[1]):
         if i in removed_features:
             continue
@@ -36,21 +39,26 @@ def preprocess_test_data(input_data, removed_features):
         cur_feature = input_data[:, i]
 
         # Replace missing entries with the mean of available entries then standardize
-        cur_feature[cur_feature == -999] = np.mean(cur_feature[cur_feature != -999])
+        cur_feature[cur_feature == -999] = means[realI]
         standardize(cur_feature)
+        realI += 1
 
         processed_data.append(cur_feature)
 
     return np.array(processed_data).T
 
 
-def load_training_data():
+def load_training_data(using_logistic_regression=False):
     yb, input_data, ids = load_csv_data("Data/train.csv")
-    processed_data, removed_features = preprocess_train_data(input_data)
-    return yb, processed_data, ids, removed_features
+    processed_data, removed_features, means = preprocess_train_data(input_data)
+
+    if using_logistic_regression:
+        yb[yb == -1] = 0
+
+    return yb, processed_data, ids, removed_features, means
 
 
-def load_test_data(removed_features):
+def load_test_data(removed_features, means):
     yb, input_data, ids = load_csv_data("Data/test.csv")
-    processed_data = preprocess_test_data(input_data, removed_features)
+    processed_data = preprocess_test_data(input_data, removed_features, means)
     return yb, processed_data, ids
