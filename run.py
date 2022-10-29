464307d779c9777ac345cc1bfb51853b33cc765b
Charbel Chucri <charbel11chucri@gmail.com>
2022-10-29 20:11:59 +0200

    Divided the data into 3 subsets and trained each independently

--- run.py
+++ run.py
@@ -1,19 +1,61 @@
 # -*- coding: utf-8 -*-
 from utils import *
 from implementations import *
-import matplotlib as plt
 
-
-def trainModel():
+def train_model_least_squares(yb, tx):
     """
-    Trains the model using the training data
-    Returns the trained weights, the MSE, the features that were not deemed useful, the mean of the kept features
+    Trains the model using Least Squares
+    Returns the trained weights, the MSE
     """
 
-    y, features, ids, removed_features, means = load_training_data()
-    tx = build_model_data(features)
-    w, mse = least_squares(y, tx)
-    return w, mse, removed_features, means
+    return least_squares(yb, tx)
+
+def train_model_logistic_regression(yb, tx):
+    """
+    Trains the model using Logistic Regression
+    Returns the trained weights, MSE
+    """
+
+    initial_weights = np.array([0.46756248, 0.82084076, 0.13473604, 0.06748474, 0.08071737,
+                                0.89997862, 0.99040634, 0.88295851, 0.56703793, 0.25140082,
+                                0.81367198, 0.48045343, 0.26640933, 0.90796936, 0.48122395,
+                                0.77356115, 0.55607271, 0.96981431, 0.29737622, 0.90175285,
+                                0.02513868, 0.08031006, 0.5847512, 0.13558202, 0.35724844,
+                                0.79922558, 0.40078367, 0.20064134, 0.22376159, 0.64714853,
+                                0.63752236])
+
+    initial_weights = initial_weights[:tx.shape[1]]
+
+    max_iters = 100
+    gamma = 0.1
+
+    return logistic_regression(yb, tx, initial_weights, max_iters, gamma)
+
+def train_model():
+    """
+    Trains the model on 3 subsets of the data
+    Returns for each subset:
+    the trained weights, the features that were not deemed useful, the mean of the kept features
+    """
+
+    (yb0, processed_data0, removed_features0, means0), \
+    (yb1, processed_data1, removed_features1, means1), \
+    (yb23, processed_data23, removed_features23, means23) = load_training_data(using_logistic_regression=True)
+
+    all_w = []
+    all_removed_features = []
+    all_means = []
+    for (yb, processed_data, removed_features, means) in [(yb0, processed_data0, removed_features0, means0),
+                                                          (yb1, processed_data1, removed_features1, means1),
+                                                          (yb23, processed_data23, removed_features23, means23)]:
+        tx = build_model_data(processed_data)
+        w, loss = train_model_logistic_regression(yb, tx)
+
+        all_w.append(w)
+        all_removed_features.append(removed_features)
+        all_means.append(means)
+
+    return all_w, all_removed_features, all_means
 
 
 def runModel():
@@ -21,42 +63,34 @@ def runModel():
     Trains the model and then run it on a test set to predict the results
     """
 
-    w, mse_train, removed_features, means = trainModel()
-    _, features, ids = load_test_data(removed_features, means)
-    tx = build_model_data(features)
-    y = np.dot(tx, w)
-    y[y < 0] = -1
-    y[y >= 0] = 1
+    all_w, all_removed_features, all_means = train_model()
+    all_processed_data, all_ids = load_test_data(all_removed_features, all_means)
 
-    create_csv_submission(ids, y, "out.txt")
+    id_prediction_pairs = []
+    for i in range(3):
+        processed_data = all_processed_data[i]
+        ids = all_ids[i]
+        w = all_w[i]
 
+        tx = build_model_data(processed_data)
 
-def plot_feature_output_relation():
-    """
-    Shows for each feature x potential relation between x and y
-    If y seems to be random w.r.t. x, the feature is not useful
-    """
+        predictions = sigmoid(tx.dot(w))
+        predictions[predictions < 0.5] = -1
+        predictions[predictions >= 0.5] = 1
 
-    y, features, ids, removed_features = load_training_data()
+        for j in range(len(ids)):
+            id_prediction_pairs.append((ids[j], predictions[j]))
 
-    features = np.sort(features, axis=0)
-    for i in range(20):
-        plt.plt(features[:, i], y)
-        plt.show()
+        print("Done with " + str(i))
 
+    id_prediction_pairs.sort()
+    ids = []
+    predictions = []
+    for j in range(len(id_prediction_pairs)):
+        ids.append(id_prediction_pairs[j][0])
+        predictions.append(id_prediction_pairs[j][1])
 
-def plot_feature_histogram():
-    """
-    Shows the most frequent values of each feature
-    """
-
-    y, features, ids, removed_features = load_training_data()
-    x = np.array([j for j in range(features.shape[0])])
-
-    features = np.sort(features, axis=0)
-    for i in range(20):
-        plt.hist(features[:, i])
-        plt.show()
+    create_csv_submission(ids, predictions, "out2.txt")
 
 
 runModel()
--- utils.py
+++ utils.py
@@ -1,7 +1,15 @@
 from helpers import *
-
+import matplotlib as plt
 
 def preprocess_train_data(input_data):
+    """
+    Does 3 things:
+    i) Finds and removes unwanted features (if they have too many missing points or std = 0)
+    ii) Replaces -999 with the mean of non -999 values
+    ii) Standardizes the data
+
+    Returns the standardized data, the removed features and the means
+    """
     processed_data = []
     removed_features = {}
     means = []
@@ -12,15 +20,15 @@ def preprocess_train_data(input_data):
 
         # Remove features with a lot of missing entries
         unavailable_cnt = np.sum(cur_feature == -999)
-        if unavailable_cnt * 1.5 > num_samples:
+        if unavailable_cnt * 1.5 > num_samples or np.std(cur_feature) == 0:
             removed_features[i] = 1
             continue
-
 
         # Replace missing entries with the mean of available entries then standardize
         curMean = np.mean(cur_feature[cur_feature != -999])
         means.append(curMean)
         cur_feature[cur_feature == -999] = curMean
+
         standardize(cur_feature)
 
         processed_data.append(cur_feature)
@@ -29,6 +37,15 @@ def preprocess_train_data(input_data):
 
 
 def preprocess_test_data(input_data, removed_features, means):
+    """
+    Does 3 things:
+    i) Removes unwanted features
+    ii) Replaces -999 with the mean found in the training phase
+    ii) Standardizes the data
+
+    Returns:
+        Processed data
+    """
     processed_data = []
 
     realI = 0
@@ -49,16 +66,130 @@ def preprocess_test_data(input_data, removed_features, means):
 
 
 def load_training_data(using_logistic_regression=False):
+    """
+    Args:
+        using_logistic_regression: if true, sets the outputs to be in {0,1}; otherwise, keep them as {-1,1}
+
+    Loads the training data and separates it into 3 subsets
+    One for PRI_jet_num=0, one for PRI_jet_num=1, one for PRI_jet_num=2 or 3
+    Then, preprocess each subset independently
+
+    Returns:
+        outputs, processed_data, a set of removed features and an array of means for each subset
+    """
+
     yb, input_data, ids = load_csv_data("Data/train.csv")
-    processed_data, removed_features, means = preprocess_train_data(input_data)
 
     if using_logistic_regression:
         yb[yb == -1] = 0
 
-    return yb, processed_data, ids, removed_features, means
+    #Divide dataset into 3 groups according to PRI_jet_num
+    yb0, yb1, yb23 = [], [], []
+    input_data0, input_data1, input_data23 = [], [], []
+
+    num_samples = input_data.shape[0]
+    for i in range(num_samples):
+        if input_data[i][22] == 0:
+            yb0.append(yb[i])
+            input_data0.append(input_data[i])
+        elif input_data[i][22] == 1:
+            yb1.append(yb[i])
+            input_data1.append(input_data[i])
+        else:
+            yb23.append(yb[i])
+            input_data23.append(input_data[i])
+
+    yb0 = np.array(yb0)
+    yb1 = np.array(yb1)
+    yb23 = np.array(yb23)
+
+    input_data0 = np.array(input_data0)
+    input_data1 = np.array(input_data1)
+    input_data23 = np.array(input_data23)
+
+    processed_data0, removed_features0, means0 = preprocess_train_data(input_data0)
+    processed_data1, removed_features1, means1 = preprocess_train_data(input_data1)
+    processed_data23, removed_features23, means23 = preprocess_train_data(input_data23)
+
+    return (yb0, processed_data0, removed_features0, means0), \
+           (yb1, processed_data1, removed_features1, means1), \
+           (yb23, processed_data23, removed_features23, means23)
+
+def load_test_data(all_removed_features, all_means):
+    """
+    Args:
+        all_removed_features: 3 sets of removed features, one for each data subset
+        all_means: 3 means, one for each data subset
+
+    Separates the test data into 3 subsets according to PRI_jet_num as for the training
+    Process each subset separately
+
+    Returns:
+        Processed data and ids for each subset
+    """
+
+    _, input_data, ids = load_csv_data("Data/test.csv")
+
+    input_data0, input_data1, input_data23 = [], [], []
+    ids0, ids1, ids23 = [], [], []
+
+    num_samples = input_data.shape[0]
+    for i in range(num_samples):
+        if input_data[i][22] == 0:
+            input_data0.append(input_data[i])
+            ids0.append(ids[i])
+        elif input_data[i][22] == 1:
+            input_data1.append(input_data[i])
+            ids1.append(ids[i])
+        else:
+            input_data23.append(input_data[i])
+            ids23.append(ids[i])
+
+    input_data0 = np.array(input_data0)
+    input_data1 = np.array(input_data1)
+    input_data23 = np.array(input_data23)
+
+    all_processed_data = []
+
+    idx = 0
+    for cur_input_data in [input_data0, input_data1, input_data23]:
+        all_processed_data.append(preprocess_test_data(cur_input_data, all_removed_features[idx], all_means[idx]))
+        idx += 1
+
+    return all_processed_data, [ids0, ids1, ids23]
 
 
-def load_test_data(removed_features, means):
-    yb, input_data, ids = load_csv_data("Data/test.csv")
-    processed_data = preprocess_test_data(input_data, removed_features, means)
-    return yb, processed_data, ids
+def check_missing_values():
+    """
+    Shows that number of missing values is related to PRI_jet_num
+    """
+    yb, input_data, ids = load_csv_data("Data/train.csv")
+
+    for i in range(input_data.shape[0]):
+        numMissing = np.sum(input_data[i] == -999)
+        if numMissing > 0:
+            print(numMissing, end=": ")
+            print(input_data[i][22])
+
+
+def plot_feature_output_relation():
+    """
+    Shows for each feature x potential relation between x and y
+    """
+    y, features, removed_features, means = load_training_data()
+    for i in range(20):
+        plt.plot(features[:, i], y)
+        plt.show()
+
+
+def plot_feature_histogram():
+    """
+    Shows the most frequent values of each feature
+    """
+
+    y, features, removed_features, means = load_training_data()
+
+    features = np.sort(features, axis=0)
+    for i in range(20):
+        plt.hist(features[:, i])
+        plt.show()
